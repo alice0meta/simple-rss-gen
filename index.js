6e898c77627ddde2a19c57cec62db6cbe22114bb
Alice Monday <alice0meta@gmail.com>
2015-12-02 19:03:14 -0800

    bugfixes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var site_root = 'http://thirdfoundation.github.io/'
 var fs = require('fs')
 var _ = require('underscore')
 var rss = require('rss')
+var path = require('path')
 
 var blgs = repo_root+'/json/blogposts'
 var out = repo_root+'/rss.xml'
@@ -16,10 +17,10 @@ var feed = new rss({
 	site_url:'http://thirdfoundation.github.io/',
 	ttl:'60',
 	})
-fs.readdirSync(blgs).map(function(slug){
+fs.readdirSync(blgs).filter(function(ι){return /\.json$/.test(ι)}).map(function(slug){
 	var ι = JSON.parse(fs.readFileSync(blgs+'/'+slug)+'')[0]
 	var t = _(ι).pick('title','date','description')
-	t.guid = t.url = site_root+'#/blog/'+slug
+	t.guid = t.url = site_root+'#/blog/'+path.basename(slug,'.json')
 	t.date = t.date? Date(t.date)+'' : '1970-01-01T00:00:00Z' // only needed because bad data
 	feed.item(t)
 	})
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cd $(dirname $(/usr/local/bin/realpath "${BASH_SOURCE[0]}"))
+# cd $(dirname $(/usr/local/bin/realpath "${BASH_SOURCE[0]}")) # no realpath on osx
+cd "$(t="${BASH_SOURCE[0]}"; while [ -h "$t" ]; do d="$(cd -P "$(dirname "$t")" && pwd)"; t="$(readlink "$t")"; [[ $t != /* ]] && t="$d/$t"; done; cd -P "$(dirname "$t")" && pwd)"
 
 { [ -d node_modules ] || npm install; } &&
 ./index.js "$@"
